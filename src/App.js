1b011bf6783614fe3099dc59d941d8ecc5eecd68
syifanurul <syifanurul99@gmail.com>
2017-12-05 21:35:18 +0700

    Commit Pertama

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//cara 1 pake variable
+//const App = () => <h1>Hello Icip</h1>
+
+//cara 2 pake class
+class App extends React.Component{
+  render(){
+    return <h1>Hello Component baru</h1>
+  }
+}
+
+export default App;
